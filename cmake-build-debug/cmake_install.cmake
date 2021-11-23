2d1a3a2da0c6e3e542950b8b2462d32ff4a13931
alex <jiahangsocket@126.com>
2021-11-23 22:58:15 +0800

    1.c study

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: D:/DevelopmentSoftWare/cWorkSpace/c-college
+# Install script for directory: D:/Development/cWorkSpace/c-college
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -32,39 +32,249 @@ if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
 endif()
 
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for the subdirectory.
-  include("D:/DevelopmentSoftWare/cWorkSpace/c-college/cmake-build-debug/zbreak_continuezbreak_continue/cmake_install.cmake")
+# Set default install directory permissions.
+if(NOT DEFINED CMAKE_OBJDUMP)
+  set(CMAKE_OBJDUMP "D:/OtherSoft/MinGW/MinGW/bin/objdump.exe")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("D:/DevelopmentSoftWare/cWorkSpace/c-college/cmake-build-debug/count_down/cmake_install.cmake")
+  include("D:/Development/cWorkSpace/c-college/cmake-build-debug/src/com.example/chapter1/continue_break/cmake_install.cmake")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("D:/DevelopmentSoftWare/cWorkSpace/c-college/cmake-build-debug/dynamic_memory_allocation/cmake_install.cmake")
+  include("D:/Development/cWorkSpace/c-college/cmake-build-debug/src/com.example/chapter1/struct/cmake_install.cmake")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("D:/DevelopmentSoftWare/cWorkSpace/c-college/cmake-build-debug/dynamic_memory_allocation_demo/cmake_install.cmake")
+  include("D:/Development/cWorkSpace/c-college/cmake-build-debug/src/com.example/chapter1/struct_use/cmake_install.cmake")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("D:/DevelopmentSoftWare/cWorkSpace/c-college/cmake-build-debug/ui_99/cmake_install.cmake")
+  include("D:/Development/cWorkSpace/c-college/cmake-build-debug/src/com.example/chapter1/tradition_array_flaw/cmake_install.cmake")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("D:/DevelopmentSoftWare/cWorkSpace/c-college/cmake-build-debug/virus_memory/cmake_install.cmake")
+  include("D:/Development/cWorkSpace/c-college/cmake-build-debug/src/com.example/chapter1/typedef/cmake_install.cmake")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("D:/DevelopmentSoftWare/cWorkSpace/c-college/cmake-build-debug/what_is_a_variable_address/cmake_install.cmake")
+  include("D:/Development/cWorkSpace/c-college/cmake-build-debug/src/com.example/chapter1/ui_99/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Development/cWorkSpace/c-college/cmake-build-debug/src/com.example/chapter1/what_is_a_variable_address/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Development/cWorkSpace/c-college/cmake-build-debug/src/com.example/chapter1/virus_memory/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Development/cWorkSpace/c-college/cmake-build-debug/src/com.example/chapter1/break_continue/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Development/cWorkSpace/c-college/cmake-build-debug/src/com.example/chapter2/bubble_sort/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Development/cWorkSpace/c-college/cmake-build-debug/src/com.example/chapter2/queue/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Development/cWorkSpace/c-college/cmake-build-debug/src/com.example/chapter2/recombination_pointer/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Development/cWorkSpace/c-college/cmake-build-debug/src/com.example/chapter2/recursion/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Development/cWorkSpace/c-college/cmake-build-debug/src/com.example/chapter2/return_break/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Development/cWorkSpace/c-college/cmake-build-debug/src/com.example/chapter2/review_function/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Development/cWorkSpace/c-college/cmake-build-debug/src/com.example/chapter2/run_multi_source/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Development/cWorkSpace/c-college/cmake-build-debug/src/com.example/chapter2/sand_clock/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Development/cWorkSpace/c-college/cmake-build-debug/src/com.example/chapter2/scanf_demo/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Development/cWorkSpace/c-college/cmake-build-debug/src/com.example/chapter2/sort_demo/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Development/cWorkSpace/c-college/cmake-build-debug/src/com.example/chapter3/message_box/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Development/cWorkSpace/c-college/cmake-build-debug/src/com.example/chapter3/array_address/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Development/cWorkSpace/c-college/cmake-build-debug/src/com.example/chapter3/pointer/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Development/cWorkSpace/c-college/cmake-build-debug/src/com.example/chapter3/pointer_and_array/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Development/cWorkSpace/c-college/cmake-build-debug/src/com.example/chapter3/pointer_demo/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Development/cWorkSpace/c-college/cmake-build-debug/src/com.example/chapter3/pointer_transfer/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Development/cWorkSpace/c-college/cmake-build-debug/src/com.example/chapter3/pointer_variable_calculator/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Development/cWorkSpace/c-college/cmake-build-debug/src/com.example/chapter3/prime_number/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Development/cWorkSpace/c-college/cmake-build-debug/src/com.example/chapter3/primitive_type_pointer/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Development/cWorkSpace/c-college/cmake-build-debug/src/com.example/chapter3/printf_demo/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Development/cWorkSpace/c-college/cmake-build-debug/src/com.example/chapter3/printlf_demo/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Development/cWorkSpace/c-college/cmake-build-debug/src/com.example/chapter4/float/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Development/cWorkSpace/c-college/cmake-build-debug/src/com.example/chapter4/float_point/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Development/cWorkSpace/c-college/cmake-build-debug/src/com.example/chapter4/formal_parameters_and_arguments/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Development/cWorkSpace/c-college/cmake-build-debug/src/com.example/chapter4/function_declaration/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Development/cWorkSpace/c-college/cmake-build-debug/src/com.example/chapter4/function_demo/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Development/cWorkSpace/c-college/cmake-build-debug/src/com.example/chapter4/graph/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Development/cWorkSpace/c-college/cmake-build-debug/src/com.example/chapter4/how_to_design_rationally_function/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Development/cWorkSpace/c-college/cmake-build-debug/src/com.example/chapter4/if_han_tower/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Development/cWorkSpace/c-college/cmake-build-debug/src/com.example/chapter4/integrated_application/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Development/cWorkSpace/c-college/cmake-build-debug/src/com.example/chapter4/linear_array/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Development/cWorkSpace/c-college/cmake-build-debug/src/com.example/chapter4/linear_array_and_pointer/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Development/cWorkSpace/c-college/cmake-build-debug/src/com.example/chapter5/constants_store/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Development/cWorkSpace/c-college/cmake-build-debug/src/com.example/chapter5/count_down/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Development/cWorkSpace/c-college/cmake-build-debug/src/com.example/chapter5/dynamic_memory_allocation/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Development/cWorkSpace/c-college/cmake-build-debug/src/com.example/chapter5/dynamic_memory_allocation_demo/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Development/cWorkSpace/c-college/cmake-build-debug/src/com.example/chapter5/dynamic_memory_cross_function/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Development/cWorkSpace/c-college/cmake-build-debug/src/com.example/chapter5/equation/cmake_install.cmake")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("D:/Development/cWorkSpace/c-college/cmake-build-debug/src/com.example/chapter5/float_and_double/cmake_install.cmake")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -75,5 +285,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "D:/DevelopmentSoftWare/cWorkSpace/c-college/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "D:/Development/cWorkSpace/c-college/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
