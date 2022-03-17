e8fab81bc12db441964cf00203832e01cb7c1ab7
alex <jiahangsocket@126.com>
2022-03-17 19:33:29 +0800

    * C prime

--- src/com.example/chapter_use_dll/UseDll.c
+++ src/com.example/chapter_use_dll/UseDll.c
@@ -6,8 +6,9 @@ typedef void(_stdcall* ExecFunc)(void);
 
 int main(){
 
-    //1.获取dll句柄。
+    //1.获取dll句柄。(公司环境和家用环境不一样)
     HMODULE dllHandle = LoadLibraryA("C:\\Users\\sxh\\CLionProjects\\c-college\\src\\com.example\\chapter_use_dll\\GenerateDll.dll");
+    //HMODULE dllHandle = LoadLibraryA("D:\\Development\\cWorkSpace\\c-college\\src\\com.example\\chapter_use_dll\\GenerateDll.dll");
     if (dllHandle == NULL) return -1;
 
     //2.获取dll句柄中的函数。
